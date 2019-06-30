d73d49f15eb70552eaba193041125be5fcebc8d3
Laur <laur.jicmon@gmail.com>
2019-06-30 14:11:44 +0200

    Initial Project Add

---
+++ src/main/java/laur/springframework/websiteexample/WebsiteExampleApplication.java
@@ -0,0 +1,13 @@
+package laur.springframework.websiteexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/laur/springframework/websiteexample/WebsiteExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package laur.springframework.websiteexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebsiteExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
