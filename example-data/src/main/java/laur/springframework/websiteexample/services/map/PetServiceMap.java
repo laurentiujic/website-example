a04fb51c2e88316a7ee7b1929171794c8ef7ff9c
Laur <laur.jicmon@gmail.com>
2019-07-05 11:34:26 +0200

    Impl Map based Services. Closes #6.

---
+++ example-data/src/main/java/laur/springframework/websiteexample/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package laur.springframework.websiteexample.services.map;
+
+import laur.springframework.websiteexample.model.Pet;
+import laur.springframework.websiteexample.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
