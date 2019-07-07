0e7fe4075a13e617e40ced2ecf295f1174f57bd8
Laur <laur.jicmon@gmail.com>
2019-07-07 16:42:06 +0200

    Refactor Map Implementation to abstract ID generation . Closes #23

--- example-data/src/main/java/laur/springframework/websiteexample/model/BaseEntity.java
+++ example-data/src/main/java/laur/springframework/websiteexample/model/BaseEntity.java
@@ -3,13 +3,13 @@ package laur.springframework.websiteexample.model;
 import java.io.Serializable;
 
 public class BaseEntity implements Serializable {
-    private long id;
+    private Long id;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 }
