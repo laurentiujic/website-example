75f7f34f65f5c65dade7442b77f1b723f5649c07
Laur <laur.jicmon@gmail.com>
2019-07-09 10:18:44 +0200

    Added petTypeService. Closes #29

---
+++ example-data/src/main/java/laur/springframework/websiteexample/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package laur.springframework.websiteexample.services;
+
+import laur.springframework.websiteexample.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
