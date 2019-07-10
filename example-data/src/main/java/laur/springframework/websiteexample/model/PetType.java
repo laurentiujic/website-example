b7ccccc9defccbffa1eca027833f1ee25181ad84
Laur <laur.jicmon@gmail.com>
2019-07-10 18:40:03 +0200

    Updated owner with JPA entity mappings. Closes #36

--- example-data/src/main/java/laur/springframework/websiteexample/model/PetType.java
+++ example-data/src/main/java/laur/springframework/websiteexample/model/PetType.java
@@ -1,6 +1,14 @@
 package laur.springframework.websiteexample.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
+
+    @Column(name = "types")
     private String name;
 
     public String getName() {
