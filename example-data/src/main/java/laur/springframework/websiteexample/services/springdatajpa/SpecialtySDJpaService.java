aab779646cd6c2cc79547f84c628582c56b33827
Laur <laur.jicmon@gmail.com>
2019-07-11 13:59:38 +0200

    Added spring data jpa service for specialty. Closes #44

---
+++ example-data/src/main/java/laur/springframework/websiteexample/services/springdatajpa/SpecialtySDJpaService.java
@@ -0,0 +1,47 @@
+package laur.springframework.websiteexample.services.springdatajpa;
+
+import laur.springframework.websiteexample.model.Speciality;
+import laur.springframework.websiteexample.repositories.SpecialtyRepository;
+import laur.springframework.websiteexample.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialtySDJpaService implements SpecialtyService {
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
