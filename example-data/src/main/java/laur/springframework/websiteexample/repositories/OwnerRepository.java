7c804e7fa634ad5a72be56cf9968422951f88c0b
Laur <laur.jicmon@gmail.com>
2019-07-11 13:29:53 +0200

    Added spring data jpa service for owners. Closes #40

--- example-data/src/main/java/laur/springframework/websiteexample/repositories/OwnerRepository.java
+++ example-data/src/main/java/laur/springframework/websiteexample/repositories/OwnerRepository.java
@@ -4,5 +4,5 @@ import laur.springframework.websiteexample.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
-
+    Owner findByLastName(String lastName);
 }
