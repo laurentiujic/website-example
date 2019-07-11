294f5741974222fd485c561e0aba2ff20dacb168
Laur <laur.jicmon@gmail.com>
2019-07-11 18:35:54 +0200

    Added Project Lombok. Closes #51

--- example-web/src/main/java/laur/springframework/websiteexample/bootstrap/DataLoader.java
+++ example-web/src/main/java/laur/springframework/websiteexample/bootstrap/DataLoader.java
@@ -65,6 +65,8 @@ public class DataLoader implements CommandLineRunner {
         owner1.setCity("Miami");
         owner1.setTelephone("123456789");
 
+
+
         Pet mikesPet = new Pet();
         mikesPet.setPetType(savedDogType);
         mikesPet.setOwner(owner1);
