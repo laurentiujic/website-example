a68fe11f0f87177879ebdfc56226ac95b0cf4bf9
Laur <laur.jicmon@gmail.com>
2019-07-11 15:03:01 +0200

    Implement Map implementation for Visit Service. Closes #46

---
+++ example-data/src/main/java/laur/springframework/websiteexample/services/springdatajpa/VisitService.java
@@ -0,0 +1,8 @@
+package laur.springframework.websiteexample.services.springdatajpa;
+
+import laur.springframework.websiteexample.model.Visit;
+import laur.springframework.websiteexample.services.CrudService;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
