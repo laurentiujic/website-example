5f121c348133b8b1bf638bcb9c6c6781331f2926
Laur <laur.jicmon@gmail.com>
2019-07-18 19:39:57 +0200

    Implemented Find Owners with some minor bugs. Closes #54

--- example-data/src/main/java/laur/springframework/websiteexample/repositories/OwnerRepository.java
+++ example-data/src/main/java/laur/springframework/websiteexample/repositories/OwnerRepository.java
@@ -3,6 +3,11 @@ package laur.springframework.websiteexample.repositories;
 import laur.springframework.websiteexample.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
     Owner findByLastName(String lastName);
+
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
